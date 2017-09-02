3c615f0dec0f427b82c11ab8af5544d3435b6281
guodong <guodong@guodongdeMacBook-Pro.local>
2017-09-02 21:15:24 +0800

    modify2

--- autoweb.sh
+++ autoweb.sh
@@ -4,8 +4,8 @@ git add .
 git commit -m "modify2"
 git push
 #ssh  root@198.74.117.101  "cd /var/www/html/Bootstrap;git pull;exit"
-./pull.sh
-#node openweb.js
+#./pull.sh
+node openweb.js
 #echo "@#@"
 #pause
 #node openweb.js
