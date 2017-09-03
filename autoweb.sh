83e3ed29faa8fba0e267a186ee655ca048a19cd5
guodong <guodong@guodongdeMacBook-Pro.local>
2017-09-03 16:01:26 +0800

    modify2

--- autoweb.sh
+++ autoweb.sh
@@ -2,8 +2,8 @@
 cd /Users/guodong/Documents/myCode/Bootstrap
 git add .
 git commit -m "modify2"
-# git push
-#ssh  root@198.74.117.101  "cd /var/www/html/Bootstrap;git pull;exit"
+git push
+ssh  root@198.74.117.101  "cd /var/www/html/Bootstrap;git pull;exit"
 #./pull.sh
 node openweb.js
 #echo "@#@"
