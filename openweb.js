450e400e429b284d2ba8ddaf1ae44403a9d3b5f0
guodong <guodong@guodongdeMacBook-Pro.local>
2017-09-04 15:58:40 +0800

    modify2

--- openweb.js
+++ openweb.js
@@ -4,5 +4,7 @@
 
 var open = require('open');
 
-//open("http://oneccc.bid/Bootstrap/src/boot1.html");
-open("./src/boot1.html");
+open("http://oneccc.bid/Bootstrap/src/boot1.html");
+// open("./src/boot1.html");
+
+// open("./src/test.html");
