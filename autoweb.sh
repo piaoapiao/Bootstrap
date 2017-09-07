77d72f61bcf58c8f7ab52b9c420490fed522f48f
guodong <guodong@guodongdeMacBook-Pro.local>
2017-09-07 09:56:00 +0800

    居中

--- autoweb.sh
+++ autoweb.sh
@@ -1,7 +1,7 @@
 #/bin/bash 
 cd /Users/guodong/Documents/myCode/Bootstrap
-echo '724108'|sudo  -S  rm -rf /Library/WebServer/Documents/Bootstrap
-echo '724108'|sudo  -S  cp -R ../Bootstrap/   /Library/WebServer/Documents/Bootstrap
+# echo '724108'|sudo  -S  rm -rf /Library/WebServer/Documents/Bootstrap
+# echo '724108'|sudo  -S  cp -R ../Bootstrap/   /Library/WebServer/Documents/Bootstrap
 
 
 git add .
@@ -9,7 +9,7 @@ git commit -m "居中"
 git push
 ssh  root@198.74.117.101  "cd /var/www/html/Bootstrap;git pull;exit"
 
-#./pull.sh
+./pull.sh
 node openweb.js
 
 
